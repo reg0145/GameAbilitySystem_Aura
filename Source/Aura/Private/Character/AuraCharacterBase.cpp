903c43715aedbd15c4bd73436d0d9b1e9b6e059a
Donghyeon <Donghyeon.study@gmail.com>
2024-08-22 14:58:24 +0900

    Character Blueprint Setup

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,15 @@
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	_weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	_weapon->SetupAttachment(GetMesh(), FName(TEXT("WeaponHandSocket")));
+	_weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
